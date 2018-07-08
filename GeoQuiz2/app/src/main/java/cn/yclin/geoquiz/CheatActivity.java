c6f6318f3250e314371b084c4a8a192d62f12eb1
ycnalin <ycnalin@gmail.com>
2018-07-08 22:56:02 +0800

    first committ

---
+++ GeoQuiz2/app/src/main/java/cn/yclin/geoquiz/CheatActivity.java
@@ -0,0 +1,83 @@
+package cn.yclin.geoquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE =
+            "cn.yclin.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN =
+            "cn.yclin.geoquiz.answer_shown";
+    private static final String KEY_ANSWER_STATE = "answer_state";
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private boolean mIsAnswerShown = false;
+    private Button mShowAnswerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        if(savedInstanceState != null){
+            mIsAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_STATE,false);
+        }
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = findViewById(R.id.answer_text_view);
+        mShowAnswerButton = findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAnswerIsTrue)
+                    mAnswerTextView.setText(R.string.true_button);
+                else
+                    mAnswerTextView.setText(R.string.false_button);
+                mShowAnswerButton.setEnabled(false);
+                setAnswerShownResult(true);
+                mIsAnswerShown = true;
+            }
+        });
+        // avoid reconfiguration data leak
+        if(mIsAnswerShown){
+            if(mAnswerIsTrue)
+                mAnswerTextView.setText(R.string.true_button);
+            else
+                mAnswerTextView.setText(R.string.false_button);
+            mShowAnswerButton.setEnabled(false);
+            setAnswerShownResult(true);
+        }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean(KEY_ANSWER_STATE, mIsAnswerShown);
+
+    }
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
+
+}
