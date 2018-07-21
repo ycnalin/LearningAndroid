a6e12dbe23a533904d02f92acc69341e7bbc8b91
ycnalin <ycnalin@gmail.com>
2018-07-21 23:08:53 +0800

    chapter 7

---
+++ Criminallntent/app/src/main/java/cn/yclin/criminallntent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package cn.yclin.criminallntent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ Criminallntent/app/src/main/java/cn/yclin/criminallntent/CrimeListFragment.java
@@ -0,0 +1,7 @@
+package cn.yclin.criminallntent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+
+}
